e5f1cda9deb95cd28d4a810f98808f9e339e26b9
Muhammad Hassan Memon <134962710+Hassan-memon009@users.noreply.github.com>
2023-09-18 13:35:59 +0500

    m

---
+++ app/src/main/java/com/example/hotelvendor/AddItem.kt
@@ -0,0 +1,11 @@
+package com.example.hotelvendor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item)
+    }
+}
